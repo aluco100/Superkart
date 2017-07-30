866fbb3130f3d4e548882a1c96a0e7ffaad7a906
Alfredo Luco <aluco100@gmail.com>
2017-07-30 15:01:44 -0400

    registro con facebook

--- Superkart/ViewController.swift
+++ Superkart/ViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 import FacebookLogin
 import FBSDKLoginKit
 import SVProgressHUD
+import HexColors
 
-class ViewController: UIViewController,FBSDKLoginButtonDelegate {
+class ViewController: UIViewController {
 
-    @IBOutlet var fbButton: FBSDKLoginButton!
+    @IBOutlet weak var fbButton: UIButton!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
@@ -21,8 +22,13 @@ class ViewController: UIViewController,FBSDKLoginButtonDelegate {
         super.viewDidLoad()
         self.view.backgroundColor = SKColors().backgroundColor
         
-        self.fbButton.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+        self.fbButton.layer.borderWidth = 1.0
+        self.fbButton.layer.borderColor = UIColor("#627aac")!.cgColor
+        self.fbButton.layer.cornerRadius = 4.0
+        self.fbButton.tintColor = UIColor.white
+        self.fbButton.backgroundColor = UIColor("#3B5998")!
+        self.fbButton.addTarget(self, action: #selector(loginFacebook), for: .touchUpInside)
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,23 +44,55 @@ class ViewController: UIViewController,FBSDKLoginButtonDelegate {
         self.navigationController?.setNavigationBarHidden(false, animated: false)
     }
     
-    
-    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
-        if ((error) != nil) {
-            // Process error
+    func loginFacebook(){
+        let loginManager = LoginManager()
+        
+        loginManager.logIn([ .publicProfile, .email ], viewController: self) { loginResult in
+            
+            switch loginResult {
+                
+            case .failed(let error):
+                
+                print(error)
+                SVProgressHUD.showError(withStatus: "No se ha podido iniciar con Facebook")
+                
+            case .cancelled:
+                
+                print("User cancelled login.")
+                
+            case .success( let permisions, _, let accessToken):
+                
+                let userMgr : UserManager = UserManager.sharedInstance
+                print(permisions)
+                
+                let req = FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"email,name"], tokenString: accessToken.authenticationToken, version: nil, httpMethod: "GET")
+                req?.start(completionHandler: { (_, result, error) in
+                    if(error == nil)
+                    {
+                        let user = result as! [String : Any]
+//                        print("result \(result)")
+                        userMgr.registerUser(email: user["email"] as! String, password: "", success: {
+                            
+                            let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+                            
+                            UIApplication.shared.keyWindow?.rootViewController = mainStoryboard.instantiateInitialViewController()
+                            
+                        }, failure: { error in
+                            print(error)
+                            SVProgressHUD.showError(withStatus: "No se ha podido iniciar con facebook")
+                        })
+                    }
+                    else
+                    {
+                        SVProgressHUD.showError(withStatus: "No se ha podido iniciar con facebook")
+                    }
+                })
+                
+            }
         }
-        else if result.isCancelled {
-            // Handle cancellations
-        }
-        else {
-            // Navigate to other view
-        }
+
     }
-    
-    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
-        //logout
-    }
-    
+        
     //MARK: - IBActions
     
     @IBAction func register(_ sender: Any) {
